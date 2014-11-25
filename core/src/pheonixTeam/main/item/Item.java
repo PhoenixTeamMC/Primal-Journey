bc353c36675ff9dd9f8bad58431f69781544c8d8
Strikingwolf <Strikingwolf2012@gmail.com>
2014-11-25 15:46:12 -0600

    Do very simple item stuff

--- core/src/pheonixTeam/main/item/Item.java
+++ core/src/pheonixTeam/main/item/Item.java
@@ -1,9 +1,24 @@
 package pheonixTeam.main.item;
 
+import pheonixTeam.main.Map;
+import pheonixTeam.main.entity.Entity;
+
 /**
  * @author Strikingwolf, chbachman
  */
 public class Item
 {
     //TODO
+
+    /**
+     * Takes params and uses the item
+     * @param entity
+     * @param map
+     * @param x
+     * @param y
+     * @param z
+     */
+    public void useItem(Entity entity, Map map, float x, float y, float z) {
+        //TODO
+    }
 }
