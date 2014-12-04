b9100d0f5e0d0832b5219d8a7419bf0b3a49e50d
Strikingwolf <Strikingwolf2012@gmail.com>
2014-12-04 13:59:16 -0600

    Get the EntityEnemy tracking features working

--- core/src/pheonixTeam/main/entity/living/enemy/EntityEnemy.java
+++ core/src/pheonixTeam/main/entity/living/enemy/EntityEnemy.java
@@ -4,14 +4,18 @@ import pheonixTeam.main.entity.Entity;
 import pheonixTeam.main.entity.living.EntityLiving;
 import pheonixTeam.main.map.Map;
 
+import java.util.ArrayList;
+
 /**
  * @author Strikingwolf, chbachman
  */
 public abstract class EntityEnemy extends EntityLiving
 {
     public Entity target;
-    public float rangeX;
-    public float rangeY;
+    public float trackRangeX;
+    public float trackRangeY;
+    public float attackRangeX;
+    public float attackRangeY;
 
     public EntityEnemy(float x, float y) {
         super(x, y);
@@ -19,6 +23,14 @@ public abstract class EntityEnemy extends EntityLiving
 
     @Override
     public void update(Map map) {
-
+        ArrayList<Entity> entitiesWithin = map.getEntitiesWithin(x, y, trackRangeX, trackRangeY);
+        ArrayList<Entity> validTargets = new ArrayList<Entity>();
+        for (Entity entity : entitiesWithin) {
+            if (!(entity instanceof EntityEnemy)) {
+                validTargets.add(entity);
+            }
+        }
+        target = map.getClosestEntityOutOf(x, y, validTargets);
+        moveToEntity(target, attackRangeX - 1, attackRangeY - 1);
     }
 }
