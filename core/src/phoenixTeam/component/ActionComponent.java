79b27aaba082cd6cb164ce2c6d6f50650e186b00
Striking <Strikingwolf2012@gmail.com>
2015-05-15 22:13:39 -0500

    /me is a derp

--- core/src/phoenixTeam/component/ActionComponent.java
+++ core/src/phoenixTeam/component/ActionComponent.java
@@ -6,6 +6,6 @@ import phoenixTeam.entity.actions.Action;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public abstract class ActionComponent extends Component{
+public class ActionComponent extends Component{
 	public HashMap<ArrayList<Integer>, Action> actions = new HashMap<ArrayList<Integer>, Action>();
 }
