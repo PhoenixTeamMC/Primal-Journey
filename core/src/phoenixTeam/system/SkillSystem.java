e55b1051076561925325d57373868d22c7c9f2c1
Striking <Strikingwolf2012@gmail.com>
2015-05-15 21:52:02 -0500

    Implement hidden skill system in SkillSytems

--- core/src/phoenixTeam/system/SkillSystem.java
+++ core/src/phoenixTeam/system/SkillSystem.java
@@ -5,6 +5,8 @@ import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
 import phoenixTeam.component.ComponentMappers;
 import phoenixTeam.component.SkillComponent;
+import phoenixTeam.entity.skills.Skill;
+import phoenixTeam.entity.skills.hidden.HiddenSkill;
 import phoenixTeam.util.KeysUtil;
 
 import java.util.ArrayList;
@@ -22,6 +24,14 @@ public class SkillSystem extends IteratingSystem {
 	@Override
 	protected void processEntity(Entity entity, float deltaTime) {
 		SkillComponent skillC = ComponentMappers.skill.get(entity);
+
+		// TODO This form should be generified somehow (take a list and get a certain instance group)
+		for (Skill s : skillC.skills.values()) {
+			if (s instanceof HiddenSkill && ((HiddenSkill) s).willUnlock(entity)) {
+				skillC.skills.put(s.keyCombo(), s);
+			}
+		}
+
 		for (ArrayList<Integer> keys : skillC.skills.keySet()) {
 			if (KeysUtil.allPressed(keys)) {
 				skillC.skills.get(keys).doSkill(entity);
